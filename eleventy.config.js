1baad961a8fe03d79683de0ebaab04f87bb98757
Paul <paul@theapplegates.com>
2024-05-11 23:41:00 -0400

    testing

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,28 +1,58 @@
-import pluginWebc from "@11ty/eleventy-plugin-webc";
-import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+// import pluginWebc from "@11ty/eleventy-plugin-webc";
+// import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+ import { eleventyImagePlugin } from "@11ty/eleventy-img";
+// const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
+// const { eleventyImagePlugin } = require("@11ty/eleventy-img");
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function(eleventyConfig) {
 	eleventyConfig.ignores.add("README.md");
 
-	eleventyConfig.addPlugin(pluginWebc, {
-		components: [
-			"./_components/**/*.webc",
-			"npm:@11ty/is-land/*.webc"
-		]
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which file extensions to process
+		extensions: "html",
+
+		// Add any other Image utility options here:
+
+		// optional, output image formats
+		formats: ["avif", "webp", "jpeg"],
+		// formats: ["auto"],
+
+		// optional, output image widths
+		// widths: ["auto"],
+
+		// optional, attributes assigned on <img> override these values.
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
 	});
 
-	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+		// Image plugin
+		eleventyConfig.addPlugin(eleventyImagePlugin, {
+			// Set global default options
+			formats: ["webp", "jpeg"],
+			urlPath: "/img/",
 
+			// Notably `outputDir` is resolved automatically
+			// to the project output directory
+
+			defaultAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+		});
 	eleventyConfig.setServerOptions({
 		domDiff: false
 	});
 
 	return {
 		dir: {
-			input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
+			input: "content", // default: "."
+			includes: "../_includes", // default: "_includes"
+			data: "../_data", // default: "_data"
 		},
 	}
 };
